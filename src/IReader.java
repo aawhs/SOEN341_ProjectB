6b37eaff308f2f86ac47ab70182309a2bc12977e
Mah514 <72531358+Mah514@users.noreply.github.com>
2020-11-17 15:33:32 -0500

    updated files

--- src/IReader.java
+++ src/IReader.java
@@ -1,2 +1,16 @@
+import java.io.*;
+import java.net.URISyntaxException;
+
 public interface IReader {
+    int linePos = 0;
+	int colPos = 0;
+	int curlinePos = 0;
+	int curcolPos = 0;
+	public void openFile() throws FileNotFoundException, URISyntaxException;
+    public void readFile() throws IOException;
+    public void scanCharacters();
+    public char getCharacter();
+    public void parseLineStmt();
+    public void parseInstruction(); // Parses all instructions from lineStatements
+
 }
