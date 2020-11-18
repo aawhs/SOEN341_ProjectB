fc9a10434bdca4b227f0b255d8112c0aff8a6223
Tabesh Haidary <haidary.tab@gmail.com>
2020-11-18 00:35:15 -0500

    Started Node class
    Very barebones at the moment, but workable.

--- src/Node.java
+++ src/Node.java
@@ -1,5 +1,39 @@
+import java.util.ArrayList;
 
 public class Node
 {
+	private ArrayList<String[]> table;
 	
+	public Node()
+	{
+		table = new ArrayList<>();
+	}
+	
+	public void addEntry(String[] entry)
+	{
+		if (entry.length != 3)
+		{
+			System.out.println("This entry's format is wrong!");
+			return;
+		}
+		
+		table.add(entry);
+	}
+	
+	public String toString()
+	{
+		String temp = "";
+		
+		for (int i = 0; i < table.size(); i++)
+		{
+			for (int j = 0; j < table.get(j).length; j++)
+			{
+				System.out.print(table.get(i)[j].concat(" "));
+			}
+			
+			System.out.println();
+		}
+		
+		return temp;
+	}
 }
