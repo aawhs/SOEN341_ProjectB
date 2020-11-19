2aea4a5a70f5ed1767327d656400a498a8754778
aawhs <ahmed.ali@mail.concordia.ca>
2020-11-19 03:12:28 -0500

    Added support for lexer, started executing methods

--- src/Lexer.java
+++ src/Lexer.java
@@ -1,65 +1,77 @@
+/*
+ * Lexer_Skeleton.java - A lexical analyser extracts tokens by reading
+ *                       characters from a source file (.asm) for the parser.
+ *
+ * @author  Michel de Champlain
+ */
 
-//public class Lexer {
-    /** Create a lexer that scans the given character stream. *//*
-    
-        public Lexer(IReader reader, ISymbolTable keywordTable) {
-            // your code...
-    
-            // Enter all mnemonics as keywords in the symbol table...
+import java.io.*;
+import java.net.URISyntaxException;
 
-            
-            IReader.linePos = 1;
-            IReader.colPos = 0;
-            IReader.curlinePos = IReader.linePos;
-            IReader.curcolPos = IReader.colPos;
-    
-            read(); // prime
-        }
-    
 
-    /* Read the next character. *//*
-    
-        private int read() {
-            colPos++;
-            return ch = reader.read();
-        }
-    
-        private void error(String t) {
-            errorReporter.record( _Error.create(t, getPosition()) );
-        }
-    
-        private void scanNumber() {
-            // your code...
-        }
-        private int scanIdentifier() {
-            // your code...
-        }
-        private int scanDirective() {
-            // your code...
-        }
-        private int scanString() {
-            // your code...
-        }
-        /*
-         * Scan the next token. Mark position on entry in case of error.
-         * @return   the token.
-         *//*
-    
-        public int getToken() {
-            // skip whitespaces
-            // "\n", "\r\n", "\n", or line comments are considered as EOL
-    
-            // your code...
-    
-            // Mark position (after skipping blanks)
-            curlinePos = linePos;
-            curcolPos = colPos;
-    
-            while (true) {
-                switch ( ch ) {
+public class Lexer implements ILexer, Opcode {
+    /** Create a lexer that scans the given character stream. */
+    public Lexer(IReader reader, ISymbolTable keywordTable) throws FileNotFoundException, URISyntaxException {
+        this.reader = reader;
+        this.keywordTable = keywordTable;
+        errorReporter = new ErrorReporter();
+
+
+
+        // Enter all mnemonics as keywords in the symbol table...
+        linePos = 1;
+        colPos = 0;
+        curlinePos = linePos;
+        curcolPos = colPos;
+        position = new Position(curlinePos, curcolPos);
+        read();
+
+    }
+
+    /* Read the next character. */
+    private int read() {
+        colPos++;
+        return this.ch = reader.read();
+    }
+
+    private void error(String t) {
+        errorReporter.record( _Error.create(t, getPosition()) );
+    }
+
+    private void scanNumber() {
+        // your code...
+    }
+    private int scanIdentifier() {
+        // your code...
+        return 0;
+    }
+    private int scanDirective() {
+        // your code...
+        return 0;
+    }
+    private int scanString() {
+        // your code...
+        return 0;
+    }
+    /**
+     * Scan the next token. Mark position on entry in case of error.
+     * @return   the token.
+     */
+    public int getToken() {
+        // skip whitespaces
+        // "\n", "\r\n", "\n", or line comments are considered as EOL
+
+        // your code...
+
+        // Mark position (after skipping blanks)
+        curlinePos = linePos;
+        curcolPos = colPos;
+
+        while (true) {
+            switch ( ch ) {
+
                 case -1:
                     return EOF;
-    
                 case 'a': case 'b': case 'c': case 'd': case 'e':
                 case 'f': case 'g': case 'h': case 'i': case 'j':
                 case 'k': case 'l': case 'm': case 'n': case 'o':
@@ -74,32 +86,54 @@
                 case '_':
                 case 'Z':
                     return scanIdentifier();
-    
-                case '.':  */
-    /* dot for directives as a first character *//*
-    
+
+                case '.':  /* dot for directives as a first character */
                     return scanDirective();
-    
-    
+
+
                 case '0': case '1': case '2': case '3': case '4':
                 case '5': case '6': case '7': case '8': case '9':
                     scanNumber();
                     return NUMBER;
-    
+
                 case '-':
                     read(); return MINUS;
-    
+
                 case ',':
                     read(); return COMMA;
-    
+
                 case '"':
                     return scanString();
-    
+
                 default:
                     read(); return ILLEGAL_CHAR;
-                }
             }
         }
     }
+
+    @Override
+    public String getTokenName(int t) {
+        return null;
+    }
+
+    private Position getPosition(){
+        return position;
+    }
+
+    private int linePos = 1;
+    private int colPos = 0;
+    private int curlinePos = linePos;
+    private int curcolPos = colPos;
+
+    private final Integer MINUS = 101,
+                            COMMA= 102 ,
+                            ILLEGAL_CHAR = 103,
+                            NUMBER= 104;
+
+    private int ch;
+    private IReader reader;
+    private IReportable errorReporter;
+    private ISymbolTable keywordTable;
+    private Position position;
+
 }
-*/
