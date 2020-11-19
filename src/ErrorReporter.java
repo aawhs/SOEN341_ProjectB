1ea0f256a53dc44eec3571d808990fe556ea154e
aawhs <ahmed.ali@mail.concordia.ca>
2020-11-19 03:10:03 -0500

    Created Error Reporter Class

--- src/ErrorReporter.java
+++ src/ErrorReporter.java
@@ -1,2 +1,7 @@
 public class ErrorReporter implements IReportable{
+
+    @Override
+    public void record(_Error e) {
+        //TODO ADD ERROR TO LIST
+    }
 }
---
+++ src/_Error.java
@@ -0,0 +1,13 @@
+public class _Error {
+    _Error(String t, Position pos){
+        this.text = t;
+        this.pos = pos;
+    }
+    static _Error create(String t, Position position){
+        _Error e = new _Error(t, position);
+        return e;
+    }
+
+    private String text;
+    private Position pos;
+}
