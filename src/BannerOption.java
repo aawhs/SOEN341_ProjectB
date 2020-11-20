34fa691b25d0c93e5508c0b99912f34d6d1f7ddc
AdamBergeron <adam.bergeron@me.com>
2020-11-20 15:38:43 -0500

    test

---
+++ src/BannerOption.java
@@ -0,0 +1,18 @@
+import static java.lang.System.exit;
+
+public class BannerOption extends Option {
+    public BannerOption() {
+        super(new String[]{"-b"},"-banner");
+        setEnable(true);
+    }
+
+    @Override
+    public void process() {
+        System.out.println("wcOO Version 1.2\n" +
+                "Copyright (C) Concordia University 2020. All Rights Reserved.\n" +
+                "Written by Ahmed Ali\n");
+
+        exit(1);
+    }
+
+}
